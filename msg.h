c629cf566274880534cdb3b863912bb76e91fc59
lengwe <44134544+lengwe@users.noreply.github.com>
2020-03-28 23:42:11 +0000

    WORKS

--- msg.h
+++ msg.h
@@ -8,11 +8,24 @@
 #include "bitcoin.h"
 #include "mbed.h"
 
+extern RawSerial pc;
 
-extern Thread thread;
+extern Thread getMessage;
 extern Thread receiveMessage;
+
+typedef struct {
+    uint8_t code;
+    float data; 
+    uint64_t data_64;
+}mail_tc;
+
+extern mail_tc mail_t;
+extern Mail<mail_tc,8> mail_box;
+extern Mail<uint8_t,8> inCharQ;
 
 extern void getmsg();
 extern void receivemsg();
+extern void putMessage(uint8_t type, float variable, uint64_t variable_64);
+extern void serialISR();
 
 #endif
