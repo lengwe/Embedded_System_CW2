4c47f99d4fb5e85834237344124086f4824f9b0f
lengwe <44134544+lengwe@users.noreply.github.com>
2020-03-28 23:41:54 +0000

    WORKS

--- msg.cpp
+++ msg.cpp
@@ -1,12 +1,14 @@
 #include "msg.h"
 
-Thread thread;
-Thread receiveMessage;
-//Thread motorCtrlT (osPriorityNormal,1024);
+RawSerial pc(SERIAL_TX, SERIAL_RX);
+
 char command[18];
 uint64_t receivedKey;
 int i = 0;
 int j = 0;
+
+Mail<mail_tc,8> mail_box;
+Mail<uint8_t,8> inCharQ;
 
 
 void getmsg(){
@@ -20,7 +22,10 @@ void getmsg(){
                 pc.printf("N%016llX\n\r",mail->data_64);
                 break;
             case(COUNT):
-                pc.printf("Hash rate is: %d\n\r",mail->data_64);
+//                pc.printf("Hash rate is: %d\n\r",mail->data_64);
+                break;
+            case(KEY):
+                pc.printf("K%016llX\n\r",mail->data_64);
                 break;
             case(MAX_VEL):
                 pc.printf("Max Velocity is %f\n\r",mail->data);
@@ -29,19 +34,33 @@ void getmsg(){
                   pc.printf("Target Rotation is: %f\n\r",mail->data);
                 break;
             case(ACT_VELOCITY):
-                pc.printf("Actual velocity is %f\n\r", mail->data);
+                pc.printf("Actual velocity is %f , ", mail->data);
                 break;
-            case(MELODY):
-//              pc.printf("")
+            case 8:
+                pc.printf("Pos_err is %f\n\r", mail->data);
                 break;
             case(ERROR):
-//                pc.printf("ERROR!!!");
+                pc.printf("ERROR!!!");
                 break;
             }
         mail_box.free(mail);
         }        
     }
     
+}
+
+void serialISR(){
+    uint8_t* newChar = inCharQ.alloc();
+    *newChar = pc.getc();
+    inCharQ.put(newChar);
+}
+
+void putMessage(uint8_t type, float variable, uint64_t variable_64){
+    mail_tc *mail = mail_box.alloc();
+    mail->code = type;
+    mail->data = variable; 
+    mail->data_64 = variable_64;
+    mail_box.put(mail);
 }
 
 
@@ -61,15 +80,12 @@ void receivemsg(){
         if(command[i]=='\r'){
 //            pc.printf("in!");
             wait_us(100);
+//            ThisThread::sleep_for(0.001);
 //            pc.printf("hex2 is %x",hex);
 
             command[i+1] = '\0' ;
             switch(command[0]){
                 case 'K':
-                    //pc.printf("K");
-//                    for(j = 0; j<i;j++){
-//                        pc.printf("%c",command[j]);
-//                        }
                     sscanf(command,"K%hx",&hex);
 //                    pc.printf("hex is %x \n\r",hex);
                     receivedKey = (uint64_t)hex;
@@ -81,7 +97,6 @@ void receivemsg(){
                 case 'V':
                     sscanf(command,"V%f",&max_velocity,0);
                     max_vel = max_velocity;
-//                    pc.printf("max_vel is %d \n\r", max_velocity);
                     putMessage(MAX_VEL, max_velocity,0);
                     break;
                 case 'R':
@@ -92,7 +107,6 @@ void receivemsg(){
                 case 'T':
                     sscanf(command,"T%s", tune);
                     note_extraction();
-//                    putMessage(ROTATE, (uint64_t)tune);
                     break;
                 }
                 i=0;
